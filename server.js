fdc7ccf7fb49357baf39d78d0535179777e321e8
nathithon <106004714+minemint@users.noreply.github.com>
2024-10-09 16:51:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
 credentials:true,
-  origin:[https://storeapi-nodejs.onrender.com],
+  origin:["https://storeapi-nodejs.onrender.com"],
 }));
 app.use(cookieParser());
 app.use(
