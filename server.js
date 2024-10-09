54cd05a0528df29bf397fa4824d57ec59b228d59
nathithon <106004714+minemint@users.noreply.github.com>
2024-10-09 16:58:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
 credentials:true,
-  origin:["https://storeapi-nodejs.onrender.com"],
+  origin:["example-frontend-github-io.onrender.com/"],
 }));
 app.use(cookieParser());
 app.use(
