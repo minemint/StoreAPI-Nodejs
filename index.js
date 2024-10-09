2aaca615965d034a26c829970e6f916bb21f80c2
nathithon <106004714+minemint@users.noreply.github.com>
2024-10-09 16:46:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+credentials:true,
+}));
 app.use(cookieParser());
 app.use(
   session({
