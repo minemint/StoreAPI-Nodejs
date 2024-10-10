d2dd8d72e9b57cccd71f203ed30c96ed6c2e54fd
nathithon <106004714+minemint@users.noreply.github.com>
2024-10-10 16:15:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ app.use(
     origin: ["https://example-frontend-github-io.onrender.com"],
   })
 );
-
-import axios from "axios";
-import express from "express";
-import bodyParser from "body-parser";
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
