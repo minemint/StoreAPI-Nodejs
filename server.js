77029a1af0e6de78ea98272bdaa695a5f80078f0
nathithon <106004714+minemint@users.noreply.github.com>
2024-10-10 17:08:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,284 +1,221 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import env from "dotenv";
+import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import db from "./database.js";
 import jwt from "jsonwebtoken";
-import passport from "passport";
-import passportJWT from "passport-jwt";
-import session from "express-session";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
-env.config();
+dotenv.config();
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: ["https://example-frontend-github-io.onrender.com"],
-  })
-);
+app.use(cors({ credentials: true, origin: ["https://example-frontend-github-io.onrender.com"] }));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
 
 const PORT = 5000;
 const saltRounds = 10;
 const secret = process.env.SECRET;
 
-//User Roles
+// User registration
 app.post("/api/register", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
   const role = "User";
+  
   try {
-    const [checkResult] = await db.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email]
-    );
+    const [checkResult] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    
     if (checkResult.length > 0) {
       return res.status(400).json({ message: "User already exists" });
-    } else {
-      const hash = await bcrypt.hash(password, saltRounds);
-      const userdata = { email: email, password: hash, role: role };
-      const [result] = await db.query("INSERT INTO users SET ?", [userdata]);
-      console.log("result", result);
-      const [user] = await db.query("SELECT * FROM users WHERE email = ?", [
-        email,
-      ]);
-      console.log("user", user);
-      res
-        .status(201)
-        .json({ user: user[0], message: "User registered successfully" });
     }
+    
+    const hash = await bcrypt.hash(password, saltRounds);
+    const userdata = { email, password: hash, role };
+    await db.query("INSERT INTO users SET ?", [userdata]);
+    
+    const [user] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    res.status(201).json({ user: user[0], message: "User registered successfully" });
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// User login
 app.post("/api/login", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
+  
   try {
-    const [result] = await db.query("SELECT * FROM users WHERE email = ? ", [
-      email,
-    ]);
+    const [result] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    
     if (result.length > 0) {
       const user = result[0];
-      const storedHashedPassword = user.password;
-      bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-        if (err) {
-          console.error("Error comparing passwords:", err);
-          res.status(500).json({ message: "Error comparing passwords:" });
-        } else {
-          if (valid) {
-            const token = jwt.sign({ email: user.email }, secret, {
-              expiresIn: "1h",
-            });
-            console.log("token: ", token);
-            console.log("user: ", user);
-            // res.cookie("token", token, {
-            //   maxAge: 300000,
-            //   secure: true,
-            //   httpOnly: true,
-            //   sameSite: "none",
-            // });
-            console.log("token: ", token);
-            console.log("user: ", user);
-            res.status(200).json({
-              user: user,
-              message: "User logged in successfully",
-              token: token,
-            });
-          } else {
-            res.json({ message: "Invalid password" });
-          }
-        }
-      });
+      
+      const valid = await bcrypt.compare(password, user.password);
+      if (valid) {
+        const token = jwt.sign({ email: user.email }, secret, { expiresIn: "1h" });
+        res.status(200).json({ user, message: "User logged in successfully", token });
+      } else {
+        res.status(401).json({ message: "Invalid password" });
+      }
+      
     } else {
       res.status(404).json({ message: "User not found" });
     }
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get("/api/logout",  (req, res) => {
-  try {
-    localStorage.removeItem("token");
+
+// User logout
+app.get("/api/logout", (req, res) => {
+  // Clear the session or token as per your implementation
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
     res.status(200).json({ message: "User logged out successfully" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-  res.json({ message: "User logged out successfully" });
+  });
 });
+
+// Fetch products
 app.get("/api/products", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM products");
-    res.status(200).json({
-      result: result,
-      status: "ok",
-      message: "Products fetched successfully",
-    });
+    const [result] = await db.query("SELECT * FROM products");
+    res.status(200).json({ result, status: "ok", message: "Products fetched successfully" });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get("/api/products/:id",async (req, res) => {
-  const id = req.params.id;
+
+// Fetch product by ID
+app.get("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  
   try {
-    const result =await db.query("SELECT * FROM products WHERE id = ?", [id]);
+    const [result] = await db.query("SELECT * FROM products WHERE id = ?", [id]);
+    
     if (result.length > 0) {
-      res.status(200).json({
-        result: result,
-        status: "ok",
-        message: "Product fetched successfully",
-      });
+      res.status(200).json({ result, status: "ok", message: "Product fetched successfully" });
     } else {
       res.status(404).json({ message: "Product not found" });
     }
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get("/api/orders", (req, res) => {});
-app.post("/api/orders", (req, res) => {});
-app.post("/api/orders/:id", (req, res) => {});
-app.delete("/api/orders/:id", (req, res) => {});
 
-//Role Admin only
+// Role Admin only - Fetch users
 app.get("/api/users", async (req, res) => {
   try {
-    const authHeder = req.headers.authorization;
-    let authToken = "";
-    if (authHeder) {
-      authToken = authHeder.split(" ")[1];
-    }
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ message: "Unauthorized" });
 
-    console.log("authToken", authToken);
+    const authToken = authHeader.split(" ")[1];
     const user = jwt.verify(authToken, secret);
-    console.log("decodedToken", user);
-    const [recheck] = await db.query("SELECT * FROM users WHERE email = ?", [
-      user.email,
-    ]);
-    console.log("recheck: ", recheck);
-    if (recheck.length > 0) {
-      if (recheck[0].role === "Admin") {
-        const result = await db.query("SELECT * FROM users");
-        console.log("result: ", result);
-        console.log("recheck: ", recheck);
-        if (recheck.length > 0) {
-          if (recheck[0].role === "Admin") {
-            const [result] = await db.query("SELECT * FROM users");
-            console.log("result: ", result);
-            res.status(200).json({
-              result: result,
-              status: "ok",
-              message: "Users fetched successfully",
-            });
-          } else {
-            res.status(401).json({ message: "Unauthorized access" });
-          }
-        } else {
-          res.status(401).json({ message: "Not found" });
-        }
-      }
+    
+    const [recheck] = await db.query("SELECT * FROM users WHERE email = ?", [user.email]);
+    
+    if (recheck.length > 0 && recheck[0].role === "Admin") {
+      const [result] = await db.query("SELECT * FROM users");
+      res.status(200).json({ result, status: "ok", message: "Users fetched successfully" });
+    } else {
+      res.status(401).json({ message: "Unauthorized access" });
     }
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.post("/api/products", (req, res) => {
-  const name = req.body.name;
-  const price = req.body.price;
-  const description = req.body.description;
-  const image = req.body.image;
-  const category = req.body.category;
+
+// Add new product (Admin only)
+app.post("/api/products", async (req, res) => {
+  const { name, price, description, image, category } = req.body;
+
   try {
-    const authHeder = req.headers.authorization;
-    let authToken = "";
-    if (authHeder) {
-      authToken = authHeder.split(" ")[1];
-    }
-    console.log("authToken", authToken);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ message: "Unauthorized" });
+
+    const authToken = authHeader.split(" ")[1];
     const user = jwt.verify(authToken, secret);
-    console.log("decodedToken", user);
-    const [recheck] = db.query("SELECT * FROM users WHERE email = ?", [
-      user.email,
-    ]);
-    if (recheck.length > 0) {
-      const productdata = {
-        name: name,
-        price: price,
-        description: description,
-        image: image,
-        category: category,
-      };
-      const [result] = db.query("INSERT INTO products SET ?", [productdata]);
-      res.status(201).json({
-        result: result,
-        status: "ok",
-        message: "Product added successfully",
-      });
+    
+    const [recheck] = await db.query("SELECT * FROM users WHERE email = ?", [user.email]);
+    if (recheck.length > 0 && recheck[0].role === "Admin") {
+      const productdata = { name, price, description, image, category };
+      await db.query("INSERT INTO products SET ?", [productdata]);
+      res.status(201).json({ status: "ok", message: "Product added successfully" });
     } else {
-      res.status(401).json({ message: "Unauthorized access  " });
+      res.status(401).json({ message: "Unauthorized access" });
     }
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error", err });
   }
 });
+
+// Update product by ID
 app.post("/api/products/:id", async (req, res) => {
-  let id = req.params.id;
-  let name = req.body.name;
-  let price = req.body.price;
-  let description = req.body.description;
-  let image = req.body.image;
-  let category = req.body.category;
+  const { id } = req.params;
+  const { name, price, description, image, category } = req.body;
+
   try {
-    const product = await db.query("SELECT * FROM products WHERE id = ?", [id]);
+    const [product] = await db.query("SELECT * FROM products WHERE id = ?", [id]);
+    
     if (product.length > 0) {
-      if (name) product.name = name;
-      if (price) product.price = price;
-      if (description) product.description = description;
-      if (image) product.image = image;
-      if (category) product.category = category;
       const productdata = {
-        name: product.name,
-        price: product.price,
-        description: product.description,
-        image: product.image,
-        category: product.category,
+        name: name || product[0].name,
+        price: price || product[0].price,
+        description: description || product[0].description,
+        image: image || product[0].image,
+        category: category || product[0].category,
       };
-      const result = db.query("UPDATE products SET ? WHERE id = ?", [
-        productdata,
-        id,
-      ]);
-      res.status(200).json({
-        result: result,
-        status: "ok",
-        message: "Product updated successfully",
-      });
+      await db.query("UPDATE products SET ? WHERE id = ?", [productdata, id]);
+      res.status(200).json({ status: "ok", message: "Product updated successfully" });
     } else {
       res.status(404).json({ message: "Product not found" });
     }
+    
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.delete("/api/products/:id", (req, res) => {});
+
+// Delete product by ID
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [result] = await db.query("DELETE FROM products WHERE id = ?", [id]);
+    
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: "Product deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
